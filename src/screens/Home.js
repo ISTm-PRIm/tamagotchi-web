3f71d7ff4187fce5212906f0a4dd8df555fe0d88
Alex Korotkov <programmerweb@yandex.ru>
2019-10-02 16:53:11 +0300

    First anime

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,29 @@
 import React from "react";
+import Lottie from "react-lottie";
+
+import hello from "../images/Walk.tgs.json";
 
 const Home = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: hello,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   return (
     <div>
       <div>Welcome To Home</div>
+
+      <Lottie
+        options={defaultOptions}
+        height={400}
+        width={400}
+        isStopped={false}
+        isPaused={false}
+      />
     </div>
   );
 };
