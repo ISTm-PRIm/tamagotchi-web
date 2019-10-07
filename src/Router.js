80c0485998b1dce73b2d6f14f29d3e8647fe11f1
Alex Korotkov <programmerweb@yandex.ru>
2019-10-07 16:45:37 +0300

    Cleanup

--- src/Router.js
+++ src/Router.js
@@ -2,9 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignInPage from "./screens/Registration";
 import Home from "./screens/Home";
-import TamagotchiHouse from "./screens/TamagotchiHouse";
 import Error404 from "./screens/Error404";
-import Rules from "./screens/Rules";
 
 const RouterConf = () => {
   return (
@@ -12,10 +10,8 @@ const RouterConf = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/home" component={Home} />
-        {/* <Route exact path="/tamagotchiHome/:id?" component={TamagotchiHouse} /> */}
         <Route path="/sign_in" component={SignInPage} />
         <Route path="/sign_up" component={SignInPage} />
-        {/* <Route path="/rulse" component={Rules} /> */}
         <Route component={Error404} />
       </Switch>
     </Router>
--- src/content/Icons.js
+++ src/content/Icons.js
@@ -11,15 +11,7 @@ import {
   faHeartbeat
 } from "@fortawesome/free-solid-svg-icons";
 
-import {
-  faTwitter,
-  faInstagram,
-  faYoutube,
-  faVk,
-  faFacebookSquare,
-  faGoogle,
-  faTwitch
-} from "@fortawesome/free-brands-svg-icons";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 export function AngleLeft() {
   return <FontAwesomeIcon icon={faAngleLeft} color={"#000000"} size="3x" />;
@@ -53,26 +45,6 @@ export function Utensils() {
   return <FontAwesomeIcon icon={faUtensils} color={"#000000"} size="1x" />;
 }
 
-// export function Instagram() {
-//   return <FontAwesomeIcon icon={faInstagram} color={BLUE} size="1x" />;
-// }
-
-// export function Twiter() {
-//   return <FontAwesomeIcon icon={faTwitter} color={BLUE} size="1x" />;
-// }
-
-// export function VK() {
-//   return <FontAwesomeIcon icon={faVk} color={BLUE} size="1x" />;
-// }
-
-// export function Facebook() {
-//   return <FontAwesomeIcon icon={faFacebookSquare} color={BLUE} size="1x" />;
-// }
-
 export function Google() {
   return <FontAwesomeIcon icon={faGoogle} color={"#ffffff"} size="1x" />;
 }
-
-// export function Twitch() {
-//   return <FontAwesomeIcon icon={faTwitch} color={BLUE} size="1x" />;
-// }
