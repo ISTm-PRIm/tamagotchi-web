fecea89dc017f5d7c7b76639e9ae81b01465a0fe
Kristina1996 <kristina.baskakova15@mail.ru>
2019-10-20 21:00:25 +0300

    Function menu redesign

--- src/components/FunctionMenu.js
+++ src/components/FunctionMenu.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import LifeBar from "./LifeBar";
 import { Question, SignOut } from "../content/Icons";
 import Modal from "./Modal";
 import Help from "./Help";
-import { BLUE, YELLOW, RED, GREEN, PURPLE } from "../content/color";
+import '../content/css/header.scss';
 
 class FunctionMenu extends React.Component {
   constructor(props) {
@@ -13,14 +11,7 @@ class FunctionMenu extends React.Component {
   }
   render() {
     return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-around",
-          backgroundColor: "#ffffff"
-        }}
-      >
+      <div className={'wrapper'}>
         <Modal isShowModal={this.state.isShowHelp}>
           <Help
             hideModal={() => {
@@ -29,101 +20,26 @@ class FunctionMenu extends React.Component {
           />
         </Modal>
 
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-around",
-            flex: 1,
-            paddingLeft: 50,
-            paddingRight: 50
-          }}
-        >
-          <div>
-            <Link
-              style={{ color: "black", textDecoration: "none" }}
-              to={`/home?room=hospital`}
-            >
-              <LifeBar
-                color={this.props.value.health > 25 ? GREEN : RED}
-                value={this.props.value.health}
-                nameValue={"Здоровье"}
-              />
-            </Link>
-          </div>
-          <div>
-            <Link
-              style={{ color: "black", textDecoration: "none" }}
-              to={`/home?room=bath`}
-            >
-              <LifeBar
-                color={this.props.value.hygiene > 20 ? BLUE : RED}
-                value={this.props.value.hygiene}
-                nameValue={"Гигиена"}
-              />
-            </Link>
-          </div>
-          <div style={{ justifyContent: "center" }}>
-            <h3
-              style={{
-                fontFamily: "GraphikLCG-Semibold",
-                textAlign: "center"
-              }}
-            >
-              {this.props.nameRoom}
-            </h3>
-
-            <div
-              style={style.button}
+        <div className={'menu-room'}>
+            <h3>{this.props.nameRoom}</h3>
+            <div className={'btn-action button pulse'}
               onClick={() => {
                 this.props.click(this.props.button.state);
               }}
             >
               {this.props.button.icon}
             </div>
-          </div>
-          <div>
-            <Link
-              style={{ color: "black", textDecoration: "none" }}
-              to={`/home?room=kitchen`}
-            >
-              <LifeBar
-                color={this.props.value.food > 20 ? YELLOW : RED}
-                value={this.props.value.food}
-                nameValue={"Питание"}
-              />
-            </Link>
-          </div>
-          <div>
-            <Link
-              style={{ color: "black", textDecoration: "none" }}
-              to={`/home?room=bedroom`}
-            >
-              <LifeBar
-                color={this.props.value.sleep > 20 ? PURPLE : RED}
-                value={this.props.value.sleep}
-                nameValue={"Сон"}
-              />
-            </Link>
-          </div>
         </div>
-        <div
-          style={{
-            border: "1px solid",
-            padding: 5,
-            margin: "0px 10px 0px 0px"
-          }}
-        >
-          <div
-            style={style.button}
+
+        <div className={'menu-right'}>
+          <div className={'help-btn'}
             onClick={() => {
               this.setState({ isShowHelp: true });
             }}
           >
             <Question />
           </div>
-          <div
-            style={style.button}
+          <div className={'sign-out'}
             onClick={() => {
               document.location.href = `/sign_in`;
             }}
@@ -131,19 +47,10 @@ class FunctionMenu extends React.Component {
             <SignOut />
           </div>
         </div>
+
       </div>
     );
   }
 }
 
 export default FunctionMenu;
-
-const style = {
-  button: {
-    // margin: 10,
-    padding: 10,
-    backgroundColor: "azure",
-    borderRadius: 20,
-    border: "1px solid"
-  }
-};
